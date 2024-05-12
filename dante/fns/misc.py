e7f8578d969d77ffd9f250aaa4f0b3a1b79b38c1
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 02:35:26 +0700

    update dante/fns/misc.py

--- dante/fns/misc.py
+++ dante/fns/misc.py
@@ -1,9 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2022 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
 import base64
 import os
@@ -14,7 +8,7 @@ from logging import WARNING
 from random import choice, randrange, shuffle
 from traceback import format_exc
 
-from Ayra.exceptions import DependencyMissingError
+from dante.exceptions import DependencyMissingError
 
 try:
     from aiohttp import ContentTypeError
@@ -69,7 +63,7 @@ except ImportError:
 
 
 async def randomchannel(
-    tochat, channel, range1, range2, caption=None, client=ayra_bot
+    tochat, channel, range1, range2, caption=None, client=dante_bot
 ):
     do = randrange(range1, range2)
     async for x in client.iter_messages(channel, add_offset=do, limit=1):
@@ -165,8 +159,8 @@ async def allcmds(event, telegraph):
         for zz in LIST[z]:
             txt += HNDLR + zz + "\n"
         txt += "\n\n"
-    t = telegraph.create_page(title="Ayra All Cmds", content=[txt])
-    await eor(event, f"All Ayra Cmds : [Click Here]({t['url']})", link_preview=False)
+    t = telegraph.create_page(title="dante All Cmds", content=[txt])
+    await eor(event, f"All dante Cmds : [Click Here]({t['url']})", link_preview=False)
 
 
 async def ReTrieveFile(input_file_name):
@@ -183,7 +177,7 @@ async def ReTrieveFile(input_file_name):
             if "image" not in contentType:
                 return False, (await out.json())
 
-            name = check_filename("ayra-rmbg.png")
+            name = check_filename("dante-rmbg.png")
             file = await aiofiles.open(name, "wb")
             await file.write(await out.read())
             await file.close()
@@ -285,8 +279,8 @@ INSTA_CLIENT = []
 
 
 async def _insta_login():
-    if "insta_creds" in ayra_bot._cache:
-        return ayra_bot._cache["insta_creds"]
+    if "insta_creds" in dante_bot._cache:
+        return dante_bot._cache["insta_creds"]
     username = udB.get_key("INSTA_USERNAME")
     password = udB.get_key("INSTA_PASSWORD")
     if username and password:
@@ -294,7 +288,7 @@ async def _insta_login():
         cl = Client(settings)
         try:
             cl.login(username, password)
-            ayra_bot._cache.update({"insta_creds": cl})
+            dante_bot._cache.update({"insta_creds": cl})
         except ManualInputRequired:
             LOGS.exception(format_exc())
             # await get_insta_code(cl, username, password)
@@ -308,14 +302,14 @@ async def _insta_login():
             return False
         udB.set_key("INSTA_SET", str(cl.get_settings()))
         cl.logger.setLevel(WARNING)
-        return ayra_bot._cache["insta_creds"]
+        return dante_bot._cache["insta_creds"]
     return False
 
 
 async def get_insta_code(username, choice):
-    from .. import asst, ayra_bot
+    from .. import asst, dante_bot
 
-    async with asst.conversation(ayra_bot.uid, timeout=60 * 2) as conv:
+    async with asst.conversation(dante_bot.uid, timeout=60 * 2) as conv:
         await conv.send_message(
             "Enter The **Instagram Verification Code** Sent to Your Email.."
         )
