c00d60b0890299c939f4d81cd237f95633de1bbd
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 02:12:24 +0700

    update assistant/manager/admins.py

--- assistant/manager/admins.py
+++ assistant/manager/admins.py
@@ -1,19 +1,13 @@
-# Ayra - UserBot
-# Copyright (C) 2021-2022 senpai80
-#
-# This file is a part of < https://github.com/senpai80/Ayra/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 
 import re
 
-from Ayra import _ayra_cache
+from dante import _dante_cache
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 
 from . import *
 
 
-@ayra_cmd(pattern="d(kick|ban)", manager=True, require="ban_users")
+@dante_cmd(pattern="d(kick|ban)", manager=True, require="ban_users")
 async def dowj(e):
     replied = await e.get_reply_message()
     if replied:
@@ -33,10 +27,10 @@ async def dowj(e):
         await e.eor(str(E))
 
 
-@callback(re.compile("cc_(.*)"), func=_ayra_cache.get("admin_callback"))
+@callback(re.compile("cc_(.*)"), func=_dante_cache.get("admin_callback"))
 async def callback_(event):
     data = event.data_match.group(1).decode("utf-8")
-    if data not in _ayra_cache.get("admin_callback", {}):
+    if data not in _dante_cache.get("admin_callback", {}):
         return
     try:
         perm = await event.client.get_permissions(event.chat_id, event.sender_id)
@@ -44,6 +38,6 @@ async def callback_(event):
         return await event.answer("Join the Group First!", alert=True)
     if not perm.is_admin:
         return await event.answer("You are not an Admin!", alert=True)
-    _ayra_cache["admin_callback"].update({data: (event.sender, perm)})
+    _dante_cache["admin_callback"].update({data: (event.sender, perm)})
     await event.answer("Verification Done!")
     await event.delete()
