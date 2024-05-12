c19b8c8194a202e0659a578d64fadc9eb94ea0e1
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 02:09:47 +0700

    update assistant/manager/__init__.py

--- assistant/manager/__init__.py
+++ assistant/manager/__init__.py
@@ -1,11 +1,11 @@
-# Ayra - UserBot
+# dante - UserBot
 # Copyright (C) 2021-2022 senpai80
 #
-# This file is a part of < https://github.com/senpai80/Ayra/ >
+# This file is a part of < https://github.com/senpai80/dante/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+# <https://www.github.com/senpai80/dante/blob/main/LICENSE/>.
 
-from Ayra._misc._decorators import ayra_cmd
-from Ayra.fns.helper import inline_mention
+from dante._misc._decorators import dante_cmd
+from dante.fns.helper import inline_mention
 
 from .. import *
