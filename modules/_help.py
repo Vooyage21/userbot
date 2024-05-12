9ef7ff544530c454d6e8d6423ad0222a2ecd9847
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 02:59:52 +0700

    update modules/_help.py

--- modules/_help.py
+++ modules/_help.py
@@ -1,18 +1,18 @@
-# Ayra - UserBot
+# dante - UserBot
 # Copyright (C) 2021-2022 senpai80
 #
-# This file is a part of < https://github.com/senpai80/Ayra/ >
+# This file is a part of < https://github.com/senpai80/dante/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+# <https://www.github.com/senpai80/dante/blob/main/LICENSE/>.
 
-from Ayra.dB._core import HELP, LIST
-from Ayra.fns.tools import cmd_regex_replace
+from dante.dB._core import HELP, LIST
+from dante.fns.tools import cmd_regex_replace
 from telethon.errors.rpcerrorlist import (BotInlineDisabledError,
                                           BotMethodInvalidError,
                                           BotResponseTimeoutError)
 from telethon.tl.custom import Button
 
-from . import HNDLR, LOGS, asst, ayra_cmd, get_string
+from . import HNDLR, LOGS, asst, dante_cmd, get_string
 
 _main_help_menu = [
     [
@@ -21,26 +21,26 @@ _main_help_menu = [
 ]
 
 
-@ayra_cmd(pattern="[hH][eE][lL][pP]( (.*)|$)")
-async def _help(ayra):
-    plug = ayra.pattern_match.group(1).strip()
-    chat = await ayra.get_chat()
+dante_cmd(pattern="[hH][eE][lL][pP]( (.*)|$)")
+async def _help(dante):
+    plug = dante.pattern_match.group(1).strip()
+    chat = await dante.get_chat()
     if plug:
         try:
             if plug in HELP["Official"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Official"][plug]:
                     output += i
-                output += "\n© @reza"
-                await ayra.eor(output)
+                output += "\n© dante"
+                await dante.eor(output)
             else:
                 try:
                     x = get_string("help_11").format(plug)
                     for d in LIST[plug]:
                         x += HNDLR + d
                         x += "\n"
-                    x += "\n© @rezadgank"
-                    await ayra.eor(x)
+                    x += "\n© dante"
+                    await dante.eor(x)
                 except BaseException:
                     file = None
                     compare_strings = []
@@ -65,25 +65,25 @@ async def _help(ayra):
                             None,
                         ):
                             text += f"\nDid you mean `{best_match}`?"
-                        return await ayra.eor(text)
+                        return await dante.eor(text)
                     output = f"**Perintah** `{plug}` **ditemukan dalam** - `{file}`\n"
                     if file in HELP["Official"]:
                         for i in HELP["Official"][file]:
                             output += i
-                    output += "\n© @reza"
-                    await ayra.eor(output)
+                    output += "\n© dante"
+                    await dante.eor(output)
         except BaseException as er:
             LOGS.exception(er)
-            await ayra.eor("Error 🤔 occured.")
+            await dante.eor("Error 🤔 occured.")
     else:
         try:
-            results = await ayra.client.inline_query(asst.me.username, "ayra")
+            results = await dante.client.inline_query(asst.me.username, "dante")
         except BotMethodInvalidError:
             z = []
             for x in LIST.values():
                 z.extend(x)
             cmd = len(z) + 10
-            return await ayra.reply(
+            return await dante.reply(
                 get_string("inline_4").format(
                     OWNER_NAME,
                     len(HELP["Official"]),
@@ -92,10 +92,10 @@ async def _help(ayra):
                 buttons=_main_help_menu,
             )
         except BotResponseTimeoutError:
-            return await ayra.eor(
+            return await dante.eor(
                 get_string("help_2").format(HNDLR),
             )
         except BotInlineDisabledError:
-            return await ayra.eor(get_string("help_3"))
-        await results[0].click(chat.id, reply_to=ayra.reply_to_msg_id, hide_via=True)
-        await ayra.delete()
+            return await dante.eor(get_string("help_3"))
+        await results[0].click(chat.id, reply_to=dante.reply_to_msg_id, hide_via=True)
+        await dante.delete()
