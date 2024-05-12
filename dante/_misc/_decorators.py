d1939351352c9ea65805e27218e8770ba8e534cd
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 02:20:57 +0700

    update dante/_misc/_decorators.py

--- dante/_misc/_decorators.py
+++ dante/_misc/_decorators.py
@@ -1,9 +1,3 @@
-# dante - UserBot
-# Copyright (C) 2021-2022 senpai80
-#
-# This file is a part of < https://github.com/senpai80/dante/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/dante/blob/main/LICENSE/>.
 
 import asyncio
 import inspect
