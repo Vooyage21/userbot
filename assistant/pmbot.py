c8cb2cf93ae4286c06796be498b0128dec5c0105
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 02:08:34 +0700

    update assistant/pmbot.py

--- assistant/pmbot.py
+++ assistant/pmbot.py
@@ -1,13 +1,4 @@
-# dante - UserBot
-# Copyright (C) 2021-2022 senpai80
-#
-# This file is a part of < https://github.com/senpai80/dante/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/dante/blob/main/LICENSE/>.
 
-# https://github.com/xditya/TeleBot/blob/master/telebot/plugins/mybot/pmbot/incoming.py
-
-# --------------------------------------- Imports -------------------------------------------- #
 
 import os
 
