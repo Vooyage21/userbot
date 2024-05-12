ae929f2d7505eba9a6154add7d48d44205758bc4
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 02:57:24 +0700

    update modules/_ayra.py

--- modules/_ayra.py
+++ modules/_ayra.py
@@ -1,12 +1,6 @@
-# Ayra - UserBot
-# Copyright (C) 2021-2022 senpai80
-#
-# This file is a part of < https://github.com/senpai80/Ayra/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
 
 
-from . import LOG_CHANNEL, Button, asst, ayra_cmd, eor, get_string
+from . import LOG_CHANNEL, Button, asst, dante_cmd, eor, get_string
 
 REPOMSG = """
 K E P O
@@ -14,23 +8,23 @@ K E P O
 
 RP_BUTTONS = [
     [
-        Button.url(get_string("bot_3"), "https://github.com/pinxRobtik/Assistant-Key"),
+        Button.url(get_string("bot_3"), "https://t.me/StreamSupportMp3"),
     ],
-    [Button.url("Support Group", "t.me/publikdarknes")],
+    [Button.url("Support", "t.me/StreamSupportMp3")],
 ]
 
-AYSTRING = """🎇 **Terima kasih telah mengunakan Reza-Userbot**
+AYSTRING = """🎇 **Terima kasih telah mengunakan dante-Userbot**
 
 • Here, are the Some Basic stuff from, where you can Know, about its Usage."""
 
 
-@ayra_cmd(pattern="repo")
-async def useAyra(rs):
+@dante_cmd(pattern="repo")
+async def usedante(rs):
     button = Button.inline("Start >>", "initft_2")
     msg = await asst.send_message(
         rs.chat_id,
         AYSTRING,
-        file="https://graph.org/file/b0ede17600df06f798774.jpg",
+        file="https://mallucampaign.in/images/img_1715419813.jpg",
         buttons=button,
     )
     if not (rs.chat_id == LOG_CHANNEL and rs.client._bot):
