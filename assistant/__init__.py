d2f97e81352ad484b8dd511876a43b18d5d0fd35
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 02:13:34 +0700

    update assistant/__init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,23 +1,23 @@
-# Ayra - UserBot
+# dante - UserBot
 # Copyright (C) 2021-2022 senpai80
 #
-# This file is a part of < https://github.com/senpai80/Ayra/ >
+# This file is a part of < https://github.com/senpai80/dante/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+# <https://www.github.com/senpai80/dante/blob/main/LICENSE/>.
 
-from Ayra import *
-from Ayra import _ayra_cache
-from Ayra._misc import owner_and_sudos
-from Ayra._misc._assistant import asst_cmd, callback, in_pattern
-from Ayra.fns.helper import *
-from Ayra.fns.tools import get_stored_file
+from dante import *
+from dante import _dante_cache
+from dante._misc import owner_and_sudos
+from dante._misc._assistant import asst_cmd, callback, in_pattern
+from dante.fns.helper import *
+from dante.fns.tools import get_stored_file
 from telethon import Button, custom
 
 from modules import ATRA_COL, InlinePlugin
 from strings import get_languages, get_string
 
-OWNER_NAME = ayra_bot.full_name
-OWNER_ID = ayra_bot.uid
+OWNER_NAME = dante_bot.full_name
+OWNER_ID = dante_bot.uid
 
 AST_PLUGINS = {}
 
