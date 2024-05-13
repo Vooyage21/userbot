7a9f210decee701e38c6afb887ba91211f9c1ccb
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 07:36:46 +0700

    update modules/sudo.py

--- modules/sudo.py
+++ modules/sudo.py
@@ -1,9 +1,9 @@
-# Ayra - UserBot
+# dante - UserBot
 # Copyright (C) 2021-2022 senpai80
 #
-# This file is a part of < https://github.com/senpai80/Ayra/ >
+# This file is a part of < https://github.com/senpai80/dante/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+# <https://www.github.com/senpai80/dante/blob/main/LICENSE/>.
 """
 ✘ **Bantuan Untuk Sudo**
 
@@ -17,40 +17,40 @@
 ◉ **Keterangan:** Daftar semua pengguna sudo.
 """
 
-from Ayra._misc import sudoers
+from dante._misc import sudoers
 from telethon.tl.types import User
 
-from . import ayra_bot, ayra_cmd, get_string, inline_mention, udB
+from . import dante_bot, dante_cmd, get_string, inline_mention, udB
 
 
-@ayra_cmd(pattern="addsudo( (.*)|$)", fullsudo=False)
-async def _(ayra):
-    inputs = ayra.pattern_match.group(1).strip()
-    if ayra.reply_to_msg_id:
-        replied_to = await ayra.get_reply_message()
+@dante_cmd(pattern="addsudo( (.*)|$)", fullsudo=False)
+async def _(dante):
+    inputs = dante.pattern_match.group(1).strip()
+    if dante.reply_to_msg_id:
+        replied_to = await dante.get_reply_message()
         id = replied_to.sender_id
         name = await replied_to.get_sender()
     elif inputs:
         try:
-            id = await ayra.client.parse_id(inputs)
+            id = await dante.client.parse_id(inputs)
         except ValueError:
             try:
                 id = int(inputs)
             except ValueError:
                 id = inputs
         try:
-            name = await ayra.client.get_entity(int(id))
+            name = await dante.client.get_entity(int(id))
         except BaseException:
             name = None
-    elif ayra.is_private:
-        id = ayra.chat_id
-        name = await ayra.get_chat()
+    elif dante.is_private:
+        id = dante.chat_id
+        name = await dante.get_chat()
     else:
-        return await ayra.eor(get_string("sudo_1"), time=5)
+        return await dante.eor(get_string("sudo_1"), time=5)
     if name and isinstance(name, User) and (name.bot or name.verified):
-        return await ayra.eor(get_string("sudo_4"))
+        return await dante.eor(get_string("sudo_4"))
     name = inline_mention(name) if name else f"`{id}`"
-    if id == ayra_bot.uid:
+    if id == dante_bot.uid:
         mmm = get_string("sudo_2")
     elif id in sudoers():
         mmm = f"{name} `sudah menjadi Pengguna SUDO ...`"
@@ -60,33 +60,33 @@ async def _(ayra):
         key.append(id)
         udB.set_key("SUDOS", key)
         mmm = f"**Ditambahkan** {name} **sebagai Pengguna SUDO**"
-    await ayra.eor(mmm, time=5)
+    await dante.eor(mmm, time=5)
 
 
-@ayra_cmd(pattern="delsudo( (.*)|$)", fullsudo=False)
-async def _(ayra):
-    inputs = ayra.pattern_match.group(1).strip()
-    if ayra.reply_to_msg_id:
-        replied_to = await ayra.get_reply_message()
+@dante_cmd(pattern="delsudo( (.*)|$)", fullsudo=False)
+async def _(dante):
+    inputs = dante.pattern_match.group(1).strip()
+    if dante.reply_to_msg_id:
+        replied_to = await dante.get_reply_message()
         id = replied_to.sender_id
         name = await replied_to.get_sender()
     elif inputs:
         try:
-            id = await ayra.client.parse_id(inputs)
+            id = await dante.client.parse_id(inputs)
         except ValueError:
             try:
                 id = int(inputs)
             except ValueError:
                 id = inputs
         try:
-            name = await ayra.client.get_entity(int(id))
+            name = await dante.client.get_entity(int(id))
         except BaseException:
             name = None
-    elif ayra.is_private:
-        id = ayra.chat_id
-        name = await ayra.get_chat()
+    elif dante.is_private:
+        id = dante.chat_id
+        name = await dante.get_chat()
     else:
-        return await ayra.eor(get_string("sudo_1"), time=5)
+        return await dante.eor(get_string("sudo_1"), time=5)
     name = inline_mention(name) if name else f"`{id}`"
     if id not in sudoers():
         mmm = f"{name} `bukan Pengguna SUDO ...`"
@@ -95,20 +95,20 @@ async def _(ayra):
         key.remove(id)
         udB.set_key("SUDOS", key)
         mmm = f"**DIHAPUS** {name} **dari Pengguna SUDO(s)**"
-    await ayra.eor(mmm, time=5)
+    await dante.eor(mmm, time=5)
 
 
-@ayra_cmd(
+@dante_cmd(
     pattern="listsudo$",
 )
-async def _(ayra):
+async def _(dante):
     sudos = sudoers()
     if not sudos:
-        return await ayra.eor(get_string("sudo_3"), time=5)
+        return await dante.eor(get_string("sudo_3"), time=5)
     msg = ""
     for i in sudos:
         try:
-            name = await ayra.client.get_entity(int(i))
+            name = await dante.client.get_entity(int(i))
         except BaseException:
             name = None
         if name:
@@ -117,7 +117,7 @@ async def _(ayra):
             msg += f"• `{i}` -> Pengguna tidak valid\n"
     m = udB.get_key("SUDO") or True
     if not m:
-        m = "[False](https://graph.org/Ayra-11-29)"
-    return await ayra.eor(
+        m = "[False](https://graph.org/dante-11-29)"
+    return await dante.eor(
         f"**SUDO MODE : {m}\n\nList of SUDO Users :**\n{msg}", link_preview=False
     )
