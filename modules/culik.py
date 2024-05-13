3b795c64d0e88c04c06e180cd53e1a210aa2f25f
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 07:46:40 +0700

    update modules/culik.py

--- modules/culik.py
+++ modules/culik.py
@@ -1,9 +1,9 @@
-# Ayra - UserBot
+# dante - UserBot
 # Copyright (C) 2021-2022 senpai80
 #
-# This file is a part of < https://github.com/senpai80/Ayra/ >
+# This file is a part of < https://github.com/senpai80/dante/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+# <https://www.github.com/senpai80/dante/blob/main/LICENSE/>.
 """
 ✘ **Bantuan Untuk Culik**
 
@@ -63,7 +63,7 @@ async def get_chatinfo(event):
     return chat_info
 
 
-@ayra_cmd(pattern="invite(?: |$)(.*)")
+@dante_cmd(pattern="invite(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -108,7 +108,7 @@ async def _(event):
 # Copyright © Team Geez - Project
 
 
-@ayra_cmd(pattern="inviteall ?(.*)")
+@dante_cmd(pattern="inviteall ?(.*)")
 async def get_users(event):
     ajgg = event.text[11:]
     chat_ajgg = ajgg.lower()
@@ -121,26 +121,26 @@ async def get_users(event):
     if not ajgg:
         return await eor(event, "`Berikan username grup...`")
 
-    ayraa = await eor(event, "`Processing....`")
+    dantea = await eor(event, "`Processing....`")
     babi = await get_chatinfo(event)
     chat = await event.get_chat()
     if event.is_private:
-        return await ayraa.edit("**Tidak bisa Menambahkan Member di sini.**")
+        return await dantea.edit("**Tidak bisa Menambahkan Member di sini.**")
 
     s = 0
     f = 0
     error = "None"
-    await ayraa.edit("`Processing...`")
+    await dantea.edit("`Processing...`")
     async for user in event.client.iter_participants(babi.full_chat.id):
         try:
             await event.client(InviteToChannelRequest(channel=chat, users=[user.id]))
             s += 1
-            await ayraa.edit(
+            await dantea.edit(
                 f"`Processing...`\n\n• **Menambahkan** `{s}` **orang** \n• **Gagal Menambahkan** `{f}` **orang**\n\n** Error:** `{error}`"
             )
         except Exception as e:
             error = str(e)
             f += 1
-    return await ayraa.edit(
+    return await dantea.edit(
         f"**Terminal Selesai** \n\n• **Berhasil Menambahkan** `{s}` **orang** \n• **Gagal Menambahkan** `{f}` **orang**"
     )
