aeca8f1b250e3bf2a9b69b3ac84564e6910eb113
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 07:45:51 +0700

    update modules/filter.py

--- modules/filter.py
+++ modules/filter.py
@@ -1,9 +1,9 @@
-# Ayra - UserBot
+# dante - UserBot
 # Copyright (C) 2021-2022 senpai80
 #
-# This file is a part of < https://github.com/senpai80/Ayra/ >
+# This file is a part of < https://github.com/senpai80/dante/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/senpai80/Ayra/blob/main/LICENSE/>.
+# <https://www.github.com/senpai80/dante/blob/main/LICENSE/>.
 
 """
 ✘ **Bantuan Untuk Filter**
@@ -21,17 +21,17 @@
 import os
 import re
 
-from Ayra.dB.filter_db import add_filter, get_filter, list_filter, rem_filter
-from Ayra.fns.tools import create_tl_btn, format_btn, get_msg_button
+from dante.dB.filter_db import add_filter, get_filter, list_filter, rem_filter
+from dante.fns.tools import create_tl_btn, format_btn, get_msg_button
 from telegraph import upload_file as uf
 from telethon.tl.types import User
 from telethon.utils import pack_bot_file_id
 
-from . import ayra_bot, ayra_cmd, events, get_string, mediainfo, udB
+from . import dante_bot, dante_cmd, events, get_string, mediainfo, udB
 from ._inline import something
 
 
-@ayra_cmd(pattern="addfil( (.*)|$)")
+@dante_cmd(pattern="addfil( (.*)|$)")
 async def af(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     wt = await e.get_reply_message()
@@ -67,10 +67,10 @@ async def af(e):
             txt, btn = get_msg_button(wt.text)
         add_filter(chat, wrd, txt, None, btn)
     await e.eor(get_string("flr_4").format(wrd))
-    ayra_bot.add_handler(filter_func, events.NewMessage())
+    dante_bot.add_handler(filter_func, events.NewMessage())
 
 
-@ayra_cmd(pattern="delfil( (.*)|$)")
+@dante_cmd(pattern="delfil( (.*)|$)")
 async def rf(e):
     wrd = (e.pattern_match.group(1).strip()).lower()
     chat = e.chat_id
@@ -80,7 +80,7 @@ async def rf(e):
     await e.eor(get_string("flr_5").format(wrd))
 
 
-@ayra_cmd(pattern="filters")
+@dante_cmd(pattern="filters")
 async def lsnote(e):
     if x := list_filter(e.chat_id):
         sd = "Filter Ditemukan Dalam Obrolan Ini Adalah\n\n"
@@ -107,4 +107,4 @@ async def filter_func(e):
 
 
 if udB.get_key("FILTERS"):
-    ayra_bot.add_handler(filter_func, events.NewMessage())
+    dante_bot.add_handler(filter_func, events.NewMessage())
