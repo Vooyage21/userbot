c7713082b004e5694efdb6de1953de34a1828cfc
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 07:32:30 +0700

    update modules/welcome.py

--- modules/welcome.py
+++ modules/welcome.py
@@ -21,9 +21,9 @@
 """
 import os
 
-from Ayra.dB.greetings_db import (add_goodbye, add_welcome, delete_goodbye,
+from dante.dB.greetings_db import (add_goodbye, add_welcome, delete_goodbye,
                                   delete_welcome, get_goodbye, get_welcome)
-from Ayra.fns.tools import create_tl_btn, format_btn, get_msg_button
+from dante.fns.tools import create_tl_btn, format_btn, get_msg_button
 from telegraph import upload_file as uf
 from telethon.utils import pack_bot_file_id
 
@@ -33,7 +33,7 @@ from ._inline import something
 Note = "\n\nNote: `{group}`, `{count}`, `{name}`, `{fullname}`, `{username}`, `{userid}` can be used as formatting parameters.\n\n"
 
 
-@ayra_cmd(pattern="savwel", groups_only=True)
+@dante_cmd(pattern="savwel", groups_only=True)
 async def setwel(event):
     x = await event.eor(get_string("com_1"))
     r = await event.get_reply_message()
@@ -77,7 +77,7 @@ async def setwel(event):
         await eor(x, get_string("grt_3"), time=5)
 
 
-@ayra_cmd(pattern="rmwel", groups_only=True)
+@dante_cmd(pattern="rmwel", groups_only=True)
 async def clearwel(event):
     if not get_welcome(event.chat_id):
         return await event.eor(get_string("grt_4"), time=5)
@@ -85,7 +85,7 @@ async def clearwel(event):
     await event.eor(get_string("grt_5"), time=5)
 
 
-@ayra_cmd(pattern="getwel", groups_only=True)
+@dante_cmd(pattern="getwel", groups_only=True)
 async def listwel(event):
     wel = get_welcome(event.chat_id)
     if not wel:
@@ -98,7 +98,7 @@ async def listwel(event):
     await event.delete()
 
 
-@ayra_cmd(pattern="savgod", groups_only=True)
+@dante_cmd(pattern="savgod", groups_only=True)
 async def setgb(event):
     x = await event.eor(get_string("com_1"))
     r = await event.get_reply_message()
@@ -142,7 +142,7 @@ async def setgb(event):
         await eor(x, get_string("grt_7"), time=5)
 
 
-@ayra_cmd(pattern="rmgod", groups_only=True)
+@dante_cmd(pattern="rmgod", groups_only=True)
 async def clearwgb(event):
     if not get_goodbye(event.chat_id):
         return await event.eor(get_string("grt_6"), time=5)
@@ -150,7 +150,7 @@ async def clearwgb(event):
     await event.eor("`Goodbye Note Deleted`", time=5)
 
 
-@ayra_cmd(pattern="getgod", groups_only=True)
+@dante_cmd(pattern="getgod", groups_only=True)
 async def listgd(event):
     wel = get_goodbye(event.chat_id)
     if not wel:
