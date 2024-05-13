2952a16c3e3ee8da00be46978b3bf753d0e51327
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-05-13 08:20:34 +0700

    Update loader.py

--- dante/startup/loader.py
+++ dante/startup/loader.py
@@ -55,6 +55,45 @@ def load_other_plugins(addons=None, pmbot=None, manager=None, vcbot=None):
         Loader(path="assistant").load(
             log=False, exclude=_ast_exc, after_load=_after_load
         )
+        
+    # for addons
+    if addons:
+        if url := udB.get_key("ADDONS_URL"):
+            subprocess.run(f"git clone -q {url} addons", shell=True)
+        if os.path.exists("addons") and not os.path.exists("addons/.git"):
+            rmtree("addons")
+        if not os.path.exists("addons"):
+            subprocess.run(
+                f"git clone -q -b {Repo().active_branch} https://github.com/Vooyage21/Addons.git addons",
+                shell=True,
+            )
+        else:
+            subprocess.run("cd addons && git pull -q && cd ..", shell=True)
+
+        if not os.path.exists("addons"):
+            subprocess.run(
+                "git clone -q https://github.com/Vooyage21/Addons.git addons",
+                shell=True,
+            )
+        if os.path.exists("addons/addons.txt"):
+            # generally addons req already there so it won't take much time
+            # subprocess.run(
+            #        "rm -rf /usr/local/lib/python3.*/site-packages/pip/_vendor/.wh*"
+            #    )
+            subprocess.run("pip3 install --no-cache-dir -q -r ./addons/addons.txt", shell=True)
+
+        _exclude = udB.get_key("EXCLUDE_ADDONS")
+        _exclude = _exclude.split() if _exclude else []
+        _in_only = udB.get_key("INCLUDE_ADDONS")
+        _in_only = _in_only.split() if _in_only else []
+
+        Loader(path="addons", key="Addons").load(
+            func=load_addons,
+            include=_in_only,
+            exclude=_exclude,
+            after_load=_after_load,
+            load_all=True,
+        )
 
     if manager:
         Loader(path="assistant/manager", key="Group Manager").load()
