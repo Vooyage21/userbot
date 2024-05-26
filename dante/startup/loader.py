544eae7ecf5b9152fc6dac210987932a6ef21b9f
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-05-27 06:11:58 +0700

    Update loader.py

--- dante/startup/loader.py
+++ dante/startup/loader.py
@@ -35,9 +35,7 @@ def _after_load(loader, module, plugin_name=""):
             except BaseException as em:
                 loader._logger.exception(em)
 
-
-def load_other_plugins(addons=None, pmbot=None, manager=None, vcbot=None):
-
+def load_other_plugins(addons=None, pmbot=None, manager=None, vcbot=None):   
     # for official
     _exclude = udB.get_key("EXCLUDE_OFFICIAL") or config("EXCLUDE_OFFICIAL", None)
     _exclude = _exclude.split() if _exclude else []
@@ -48,18 +46,61 @@ def load_other_plugins(addons=None, pmbot=None, manager=None, vcbot=None):
     Loader().load(include=_in_only, exclude=_exclude, after_load=_after_load)
 
     # for assistant
-    if not udB.get_key("DISABLE_AST_PLUGINS"):
+    if not USER_MODE and not udB.get_key("DISABLE_AST_PLUGINS"):
         _ast_exc = ["pmbot"]
         if _in_only and "games" not in _in_only:
             _ast_exc.append("games")
         Loader(path="assistant").load(
             log=False, exclude=_ast_exc, after_load=_after_load
         )
-        
 
-    if manager:
-        Loader(path="assistant/manager", key="Group Manager").load()
+    # for addons
+    if addons:
+        if url := udB.get_key("ADDONS_URL"):
+            subprocess.run(f"git clone -q {url} addons", shell=True)
+        if os.path.exists("Addons") and not os.path.exists("Addons/.git"):
+            rmtree("Addons")
+        if not os.path.exists("addons"):
+            subprocess.run(
+                f"git clone -q -b {Repo().active_branch} https://github.com/Vooyage21/Addons.git Addons",
+                shell=True,
+            )
+        else:
+            subprocess.run("cd Addons && git pull -q && cd ..", shell=True)
 
-    # chat via assistant
-    if pmbot:
-        Loader(path="assistant/pmbot.py").load_single(log=False)
+        if not os.path.exists("addons"):
+            subprocess.run(
+                "git clone -q https://github.com/Vooyage21/Addons.git Addons",
+                shell=True,
+            )
+        if os.path.exists("addons/addons.txt"):
+            # generally addons req already there so it won't take much time
+            # subprocess.run(
+            #        "rm -rf /usr/local/lib/python3.*/site-packages/pip/_vendor/.wh*"
+            #    )
+            subprocess.run(
+                f"{sys.executable} -m pip install --no-cache-dir -q -r ./Addons/Addons.txt",
+                shell=True,
+            )
+
+        _exclude = udB.get_key("EXCLUDE_ADDONS")
+        _exclude = _exclude.split() if _exclude else []
+        _in_only = udB.get_key("INCLUDE_ADDONS")
+        _in_only = _in_only.split() if _in_only else []
+
+        Loader(path="addons", key="Addons").load(
+            func=load_addons,
+            include=_in_only,
+            exclude=_exclude,
+            after_load=_after_load,
+            load_all=True,
+        )
+
+    if not USER_MODE:
+        # group manager
+        if manager:
+            Loader(path="assistant/manager", key="Group Manager").load()
+
+        # chat via assistant
+        if pmbot:
+            Loader(path="assistant/pmbot.py").load(log=False)
